a2147cdea7e57e7a703642e1d1945814357074c4
Виталий Старцев <6asm6deus6@gmail.com>
2025-07-09 06:57:58 +0300

    feat: react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { createRoot } from 'react-dom/client'
 import HomePage from './components/pages/Home'
 import { QueryProvider } from './context/query/context'
 import './index.scss'
 
-createRoot(document.getElementById('root')!).render(
-    <QueryProvider>
-        <HomePage />
-    </QueryProvider>,
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+import ReactDOM from 'react-dom/client'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: (
+            <QueryProvider>
+                <HomePage />
+            </QueryProvider>
+        ),
+    },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <RouterProvider router={router} />,
 )
